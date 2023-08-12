411286e5cda97f492056ec386ab6a540042853bc
brosnanyuen <brosnanyuen@gmail.com>
2023-08-12 01:17:39 -0700

    save

--- tests/test_Linear_ODE.rs
+++ tests/test_Linear_ODE.rs
@@ -10,7 +10,7 @@ const DEVICE: i32 = 0;
 
 
 #[test]
-fn test_ODE() {
+fn test_Linear_ODE() {
 
 	arrayfire::set_backend(BACK_END);
 	arrayfire::set_device(DEVICE);
@@ -74,12 +74,97 @@ fn test_ODE() {
 
 	//Error Analysis
 	let actualy = 2.0f64 - arrayfire::cos(&t);
-	let error = y - actualy;
+	let mut error = y - actualy;
 	//arrayfire::print_gen("error".to_string(), &error,Some(6));
 
+	error = arrayfire::abs(&error);
     let (maxerr,_) =  arrayfire::max_all(&error);
 
 	//println!("maxerr: {:.20?}",maxerr);
     assert!(maxerr  <= 1E-9);
+
+
+
+
+
+
+
+
+
+
+
+
+	arrayfire::set_backend(BACK_END);
+	arrayfire::set_device(DEVICE);
+
+	// Set the Linear Differentail Equation
+	// dy/dt = 2*y-t
+	let diffeq = |t: &arrayfire::Array<f64>, y: &arrayfire::Array<f64>| -> arrayfire::Array<f64> {
+		2.0f64*y.clone()-t.clone()
+	};
+
+	//Start at t=0 and end at t=1000
+	//Step size of 0.001
+	//Relative error of 1E-9
+	//Absolute error of 1E-9
+	let options: RayBNN_DiffEq::ODE::ODE45::ODE45_Options<f64> = RayBNN_DiffEq::ODE::ODE45::ODE45_Options {
+		tstart: 0.0f64,
+		tend: 5.0f64,
+		tstep: 0.00001f64,
+		rtol: 1.0E-9f64,
+	    atol: 1.0E-9f64,
+		normctrl: false
+	};
+
+	let t_dims = arrayfire::Dim4::new(&[1,1,1,1]);
+	let mut t = arrayfire::constant::<f64>(0.0,t_dims);
+
+	let y0_dims = arrayfire::Dim4::new(&[1,1,1,1]);
+	let mut y = arrayfire::constant::<f64>(0.0,y0_dims);
+	let mut dydt = arrayfire::constant::<f64>(0.0,y0_dims);
+
+	//Initial Point of Differential Equation
+	//Set y(t=0) = 1.0
+	let y0 = arrayfire::constant::<f64>(1.0,y0_dims);
+
+	println!("Running");
+
+	arrayfire::sync(DEVICE);
+	let starttime = std::time::Instant::now();
+
+	//Run Solver
+	RayBNN_DiffEq::ODE::ODE45::linear_ode_solve(
+		&y0
+		,diffeq
+		,&options
+		,&mut t
+		,&mut y
+		,&mut dydt
+	);
+
+	arrayfire::sync(DEVICE);
+
+	let elapsedtime = starttime.elapsed();
+	
+	arrayfire::sync(DEVICE);
+
+	//arrayfire::print_gen("y".to_string(), &y,Some(6));
+	//arrayfire::print_gen("t".to_string(), &t,Some(6));
+
+	//println!("Computed {} Steps In: {:.6?}", y.dims()[0],elapsedtime);
+
+
+	//Error Analysis
+	let twot = 2.0f64*t.clone();
+	let actualy = 0.5f64*t.clone() + 0.75f64*arrayfire::exp(&twot) + 0.25f64;
+	let mut error = y - actualy;
+	//arrayfire::print_gen("error".to_string(), &error,Some(6));
+
+	error = arrayfire::abs(&error);
+    let (maxerr,_) =  arrayfire::max_all(&error);
+
+	println!("t: {}",t.dims()[0]);
+	println!("maxerr: {:.20?}",maxerr);
+    assert!(maxerr  <= 1E-6);
 
 }
