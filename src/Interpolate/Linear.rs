a4234615efeced042320f26071ccdf119bba40d9
brosnanyuen <brosnany@uvic.ca>
2023-09-06 14:13:53 -0700

    save

--- src/Interpolate/Linear.rs
+++ src/Interpolate/Linear.rs
@@ -14,7 +14,43 @@ pub fn run<Z: arrayfire::FloatingPoint>(
 	,f: &arrayfire::Array<Z>
 	,dfdt: &arrayfire::Array<Z>
 	,s: &arrayfire::Array<Z>)
-	-> arrayfire::Array<Z>  {
+	-> arrayfire::Array<Z>  
+	{
+
+	println!("t.dims()[0] {}", t.dims()[0]);
+	println!("t.dims()[1] {}", t.dims()[1]);
+
+	println!("f.dims()[0] {}", f.dims()[0]);
+	println!("f.dims()[1] {}", f.dims()[1]);
+
+	println!("dfdt.dims()[0] {}", dfdt.dims()[0]);
+	println!("dfdt.dims()[1] {}", dfdt.dims()[1]);
+
+	println!("s.dims()[0] {}", s.dims()[0]);
+	println!("s.dims()[1] {}", s.dims()[1]);
+
+	let t_dims = t.dims();
+	let input_size = t_dims[1];
+
+	let s_dims = s.dims();
+	let target_size = s_dims[1];
+
+	let f_dims = f.dims();
+	let function_size = f_dims[0];
+
+
+	let s_arr = arrayfire::transpose(&s,false);
+
+	let mut dist = s_arr - t.clone();
+
+	dist = arrayfire::abs(&dist).cast::<Z>();
+	let (_,idx) = arrayfire::imin(&dist,1);
+	drop(dist);
+
+
+
+
+
 	
 	let t = arrayfire::transpose(t, false);
 	let f = arrayfire::transpose(f, false);
